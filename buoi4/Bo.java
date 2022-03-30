deeb9457a660beb04da1425fce1265f0b6a92a68
DN| Nại Hà <89713168+Nai-Ha@users.noreply.github.com>
2022-03-30 16:00:41 +0700

    yes

--- buoi4/Bo.java
+++
@@ -1,8 +0,0 @@
-package buoi4;
-
-public class Bo extends ConVat {
-	public void keu() {
-		System.out.println("um...bo!");
-	}
-
-}
--- buoi4/Ga.java
+++
@@ -1,8 +0,0 @@
-package buoi4;
-
-public class Ga extends ConVat {
-	public void keu() {
-		System.out.println("o..o...o!");
-	}
-
-}
