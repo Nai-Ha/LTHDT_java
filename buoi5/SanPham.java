bf5faea693d1ec10a77e4528612eac4a8a1a4ecc
Nai-Ha <89713168+Nai-Ha@users.noreply.github.com>
2022-04-20 15:08:46 +0700

    Update SanPham.java

--- buoi5/SanPham.java
+++ buoi5/SanPham.java
@@ -3,24 +3,25 @@ package buoi5;
 import java.util.Scanner;
 
 public class SanPham {
-	private String maso, ten, nsx;
+	private String maso, ten;
+	private Date nsx;
 
 	public SanPham() {
 		maso = new String();
 		ten = new String();
-		nsx = new String();
+		nsx = new Date();
 	}
 
-	public SanPham(String maso1, String ten1, String nsx1) {
+	public SanPham(String maso1, String ten1, Date nsx1) {
 		maso = new String(maso1);
 		ten = new String(ten1);
-		nsx = new String(nsx1);
+		nsx = new Date(nsx1);
 	}
 
 	public SanPham(SanPham a) {
 		maso = new String(a.maso);
 		ten = new String(a.ten);
-		nsx = new String(a.nsx);
+		nsx = new Date(a.nsx);
 	}
 
 	public void nhap() {
@@ -30,7 +31,7 @@ public class SanPham {
 		System.out.println("Nhap vao ten san pham: ");
 		ten = sc.nextLine();
 		System.out.println("Nhap vao ngay san xuat san pham: ");
-		nsx = sc.nextLine();
+		nsx.nhap();
 
 	}
 
@@ -43,6 +44,4 @@ public class SanPham {
 		return ("[" + maso + "," + ten + "," + nsx + "]");
 
 	}
-
-
 }
